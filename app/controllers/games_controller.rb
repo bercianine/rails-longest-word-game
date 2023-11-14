2fd794a5abf53c53af543189622178e5881c5bba
Sofia Bercianine <sfbercianine@gmail.com>
2023-11-14 11:28:16 +0100

    added the score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,10 @@
 class GamesController < ApplicationController
+  before_action :initialize_score, only: [:new]
+
   def new
     random = ('a'..'z').to_a
     session[:letters] = random.sample(10)
+    @score = session[:score]
   end
 
   def score
@@ -13,10 +16,17 @@ class GamesController < ApplicationController
       @message = "🎉 Congrats"
       @answer = answer
       @result = "matches the letters!"
+      session[:score] += 1
     else
       @message = "😔 Sorry"
       @answer = answer
       @result = "can't be built with these letters."
     end
+  end
+
+  private
+
+  def initialize_score
+    session[:score] ||= 0
   end
 end
